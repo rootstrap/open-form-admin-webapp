f1b09ea3d883906e3bc5d322c7830aa24bb6ef3c
Manuel Cheveste <manuelcheveste@gmail.com>
2019-10-30 12:03:53 -0300

    Will use react test renderer for dom testing Enzyme is causing trouble with hooks

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -1,6 +1,1 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-require('dotenv').config({ path: '.env.test'});
-
-Enzyme.configure({ adapter: new Adapter() });
+require('dotenv').config({ path: '.env.test' });
