b376d28c0831763709c595426abe3d26e1fa0f5b
Manuel Cheveste <manuelcheveste@gmail.com>
2019-11-06 14:33:37 -0300

    Delete icon and form list styles

--- src/components/forms/FormsListItem.js
+++ src/components/forms/FormsListItem.js
@@ -1,7 +1,36 @@
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import ListItem from 'components/common/ListItem';
+import DeleteIcon from 'components/common/DeleteIcon';
 
-export default styled(ListItem)`
+const StyledListItem = styled(ListItem)`
   color: ${({ theme }) => theme.color.primary};
+  box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.15);
+  border-radius: 8px;
+  padding: 1rem 2rem;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > div:first-child {
+    width: 70%;
+  }
 `;
+
+const FormsListItem = ({ children }) => (
+  <StyledListItem>
+    <div>{children}</div>
+    <div>
+      <DeleteIcon />
+    </div>
+  </StyledListItem>
+);
+
+FormsListItem.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default FormsListItem;
