007c744b8335c7e4eeb4236cf1569be402de2b62
Manuel Cheveste <manuelcheveste@gmail.com>
2019-11-08 15:21:12 -0300

    Added middleware to handle requests reducing boilerplate and handle pagination

--- src/selectors/formSelectors.js
+++
@@ -1,7 +0,0 @@
-export const getFormCategories = ({ forms }) => forms.categories;
-
-export const getFormsByCategory = ({ forms }) => forms.byId;
-
-export const getFormError = ({ forms }) => forms.error;
-
-export const getFormIsFetching = ({ forms }) => forms.isFetching;
